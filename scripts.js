e94e9be92002083d188225eaf08a19dcbf757468
Siftben <yobel.oktavinus98@gmail.com>
2023-09-07 19:13:03 +0900

    Working buttons for number and delete and clear

--- scripts.js
+++ scripts.js
@@ -1,6 +1,57 @@
+// Stack class
+class Stack {
+ 
+  // Array is used to implement stack
+  constructor()
+  {
+      this.items = [];
+  }
+
+  // push(item)
+  push(element)
+  {
+      // push element into the items
+      if(this.items.length > 12) return;
+      this.items.push(element);
+  }
+  // pop()
+  pop()
+  {
+      // return top most element in the stack
+      // and removes it from the stack
+      // Underflow if stack is empty
+      if (this.items.length == 0)
+          return "Underflow";
+      return this.items.pop();
+  }
+  // peek()
+  peek()
+  {
+      // return the top most element from the stack
+      // but does'nt delete it.
+      return this.items[this.items.length - 1];
+  }
+  // isEmpty()
+  isEmpty()
+  {
+      // return true if stack is empty
+      return this.items.length == 0;
+  }
+  // printStack()
+  printStack()
+  {
+      var str = "";
+      for (var i = 0; i < this.items.length; i++)
+          str += this.items[i];
+      return str;
+  }
+}
+
 const number = document.querySelectorAll('.number-button');
+const clearButton = document.querySelector('.clear-button');
+const deleteButton = document.querySelector('.delete-button');
 const screen = document.querySelector('.calculate');
-console.log(number);
+var stack = new Stack();
 
 const add = function(number1, number2) {
 	return number1 + number2; 
@@ -31,8 +82,35 @@ const divide = function(array) {
 };
 
 function pressNumber() {
-  console.log(this.value);
-  document.getElementById('screen').innerHTML = this.value;
+  //document.getElementById('screen').innerHTML = this.value;
+  addStack(this.value);
+}
+
+function refreshScreen() {
+
+  if(stack.isEmpty()) {
+    document.getElementById('screen').innerHTML = '0';
+  } else
+  document.getElementById('screen').innerHTML = stack.printStack();
+
+}
+
+function addStack(value) {
+  stack.push(value);
+  refreshScreen();
+}
+
+function clearStack() {
+  stack = new Stack();
+  refreshScreen();
+}
+
+function deleteStack(){
+  stack.pop();
+  refreshScreen();
 }
 
 number.forEach(number => number.addEventListener('click' , pressNumber));
+
+clearButton.addEventListener('click', clearStack);
+deleteButton.addEventListener('click', deleteStack);
