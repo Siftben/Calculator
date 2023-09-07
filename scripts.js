9beb60aac0f75fd2c82c991221110bcfbf7647f6
Siftben <yobel.oktavinus98@gmail.com>
2023-09-07 17:58:54 +0900

    Add button event listener

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
-
-
+const number = document.querySelectorAll('.number-button');
+const screen = document.querySelector('.calculate');
+console.log(number);
 
 const add = function(number1, number2) {
 	return number1 + number2; 
@@ -18,3 +19,20 @@ const multiply = function(array) {
 
   return result;
 };
+
+const divide = function(array) {
+  let result = array.reduce(multiple);
+
+  function multiple(total, num) {
+    return total * num;
+  }
+
+  return result;
+};
+
+function pressNumber() {
+  console.log(this.value);
+  document.getElementById('screen').innerHTML = this.value;
+}
+
+number.forEach(number => number.addEventListener('click' , pressNumber));
