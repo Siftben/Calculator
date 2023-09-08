56d73768453f4b35d12c2a4cfdb6150d8aab30e1
Siftben <yobel.oktavinus98@gmail.com>
2023-09-09 02:04:11 +0900

    Working Calculator

--- scripts.js
+++ scripts.js
@@ -47,81 +47,6 @@ class Stack {
   }
 }
 
-    // Javascript program to evaluate value of a postfix
-    // expression having multiple digit operands
-     
-    // Method to evaluate value of
-    // a postfix expression
-    function evaluatePostfix(exp)
-    {
-        // create a stack
-        let stack = [];
- 
-        // Scan all characters one by one
-        for (let i = 0; i < exp.length; i++)
-        {
-            let c = exp[i];
- 
-            if (c == ' ')
-            {
-                continue;
-            }
- 
-            // If the scanned character is an
-            // operand (number here),extract
-            // the number. Push it to the stack.
-            else if (c >= '0' && c <= '9')
-            {
-                let n = 0;
- 
-                // extract the characters and
-                // store it in num
-                while (c >= '0' && c <= '9')
-                {
-                    n = n * 10 + (c - '0');
-                    i++;
-                    c = exp[i];
-                }
-                i--;
- 
-                // push the number in stack
-                stack.push(n);
-            }
- 
-            // If the scanned character is
-            // an operator, pop two elements
-            // from stack apply the operator
-            else
-            {
-                let val1 = stack.pop();
-                let val2 = stack.pop();
- 
-                switch (c)
-                {
-                    case '+':
-                    stack.push(val2 + val1);
-                    break;
- 
-                    case '-':
-                    stack.push(val2 - val1);
-                    break;
- 
-                    case '/':
-                    stack.push(parseInt(val2 / val1, 10));
-                    break;
- 
-                    case '*':
-                    stack.push(val2 * val1);
-                    break;
-                }
-            }
-        }
-        return stack.pop();
-    }
-     
-    let exp = "100 200 + 2 / 5 * 7 +";
-    console.log(evaluatePostfix(exp));
-
 const numberButton = document.querySelectorAll('.number-button');
 const operandButton = document.querySelectorAll('.operation-button');
 const resultButton = document.querySelector('.result-button');
@@ -129,50 +54,82 @@ const clearButton = document.querySelector('.clear-button');
 const deleteButton = document.querySelector('.delete-button');
 const screen = document.querySelector('.calculate');
 var stack = new Stack();
+var stack2 = new Stack();
+let result;
+let operand;
 
-const add = function(number1, number2) {
+function sum(number1, number2) {
 	return number1 + number2; 
 };
 
-const subtract = function(number1, number2) {
+function substract(number1, number2) {
 	return number1 - number2; 
 };
 
-const multiply = function(array) {
-  let result = array.reduce(multiple);
-
-  function multiple(total, num) {
-    return total * num;
-  }
-
-  return result;
+function multiply(number1, number2) {
+  return number1 * number2;
 };
 
-const divide = function(array) {
-  let result = array.reduce(multiple);
-
-  function multiple(total, num) {
-    return total * num;
-  }
-
-  return result;
+function divide(number1, number2) {
+  return number1 / number2;
 };
 
 function pressNumber() {
-  //document.getElementById('screen').innerHTML = this.value;
   addStack(this.value);
 }
 
+function pressOperand() {
+  operand = this.value;
+  stack2 = stack;
+  document.getElementById('result').innerHTML = stack.printStack() + operand;
+
+  refreshScreen();
+  stack = new Stack();
+}
+
 function showResult() {
-  let exp = stack.printStack();
-  console.log(exp);
-  console.log(evaluatePostfix(stack.printStack()));
+  result = operateFunction();
+  console.log(result);
+  document.getElementById('result').innerHTML = stack2.printStack() + operand + stack.printStack();
+  document.getElementById('screen').innerHTML = result;
+  stack = new Stack();
+}
+
+function operateFunction() {
+  let number1 = parseInt(stack2.printStack());
+  let number2 = parseInt(stack.printStack());
+
+  switch (operand) {
+
+    case '+':
+      return sum(number1, number2);
+      break;
+
+    case '-':
+      return substract(number1, number2);
+      break;
+
+    case '*':
+      return multiply(number1, number2);
+      break;
+    
+    case '/':
+      return divide(number1, number2);
+      break;
+    
+    default:
+      break;
+  }
+
 }
 
 function refreshScreen() {
-
+  if(stack.isEmpty() && stack2.isEmpty){
+    document.getElementById('result').innerHTML = '0';
+  }
   if(stack.isEmpty()) {
     document.getElementById('screen').innerHTML = '0';
+    
   } else
   document.getElementById('screen').innerHTML = stack.printStack();
 
@@ -185,6 +142,7 @@ function addStack(value) {
 
 function clearStack() {
   stack = new Stack();
+  stack2 = new Stack();
   refreshScreen();
 }
 
@@ -194,7 +152,7 @@ function deleteStack(){
 }
 
 numberButton.forEach(number => number.addEventListener('click' , pressNumber));
-operandButton.forEach(operand => operand.addEventListener('click' , pressNumber));
+operandButton.forEach(operand => operand.addEventListener('click' , pressOperand));
 resultButton.addEventListener('click', showResult);
 
 clearButton.addEventListener('click', clearStack);
